4512863ffec1bc54eb559d536bb97614edecbf11
Natalio666Altube <natalio.altube@gmail.com>
2020-07-09 21:30:31 +0200

    Covid19

--- week4/day1/delivery/your_module.py
+++ week4/day1/delivery/your_module.py
@@ -2,6 +2,12 @@
 # Write the code...
 import numpy as np 
 
+
+def hola():
+    return "hola"
+
+
+
 def mean_visualization():
     """ Draw the mean in a plot """
     return None
@@ -11,23 +17,32 @@ def revert(x):
 
 
 def tamano(array):
-    if (array.size == 0):
+    if array.size == 0:
         print("The given Array is empty")
     else:
         print("The array = ", array) 
- 
 
+"""
 x = np.array([1, 2, 3])
 y = np.array([3, 2, 1])
 np.concatenate([x, y])
+"""
 
 def array1(x,y):
     return np.concatenate([x,y])
-
+"""
 f = np.array([1, 2, 3])
 z = np.array([3, 2, 1])
 g = array1(x=f, y=z)
 print(g)
+"""
+
+def addition(a,b):
+    return a + b
 
 
+def moves(transporte):
+    return moves.choice(transporte)    
 
+def powers(x):
+    return x, x**2, x**3, x**4, x**5, x**6
